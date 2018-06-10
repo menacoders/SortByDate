8d6b3a3cc55cebc621cac786187791897b78343f
MagentoAr <7216926+MagentoAr@users.noreply.github.com>
2018-06-10 23:50:29 +0300

    Add files via upload

---
+++ registration.php
@@ -0,0 +1,11 @@
+<?php
+/*
+ * Copyright © 2018 Mohammed Ibrahim. All rights reserved.
+	* geek.php@hotmail.com
+ */
+	
+\Magento\Framework\Component\ComponentRegistrar::register(
+    \Magento\Framework\Component\ComponentRegistrar::MODULE,
+    'Mohdibra_SortByDate',
+    __DIR__
+);
